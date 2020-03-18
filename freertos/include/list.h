a2eb49680fd38287a9c41db59ae6bd31fd86f3df
wyluo <fortwater969@163.com>
2020-03-18 22:55:34 +0800

    链表

--- freertos/include/list.h
+++ freertos/include/list.h
@@ -3,14 +3,39 @@
 
 #include "portmacro.h"
 
+/*链表节点数据结构定义*/
 struct xLIST_ITEM
 {
-    TickType_t xItemValue;      
-	struct xLIST_ITEM * pxNext;
-	struct xLIST_ITEM * pxPrevious;
-	void * pvOwner;
-	void * pvContainer;
+    TickType_t xItemValue;                /* 辅助值，用于帮助节点做顺序排列*/      
+	struct xLIST_ITEM * pxNext;           /* 指向链表下一个节点 */ 
+	struct xLIST_ITEM * pxPrevious;       /* 指向链表前一个节点 */
+	void * pvOwner;                       /* 指向拥有该节点的内核对象，通常是TCB */
+	void * pvContainer;                   /* 指向该节点所在的链表    */
 };
-typedef struct xLIST_ITEM ListItem_t;
+typedef struct xLIST_ITEM ListItem_t;  /* 节点数据类型重定义 */
+
+/*链表根节点数据结构定义*/
+typedef struct xLIST
+{
+    UBaseType_t uxNumberOfItems;          /* 链表节点计数器 */
+	ListItem_t * pxIndex;                 /* 链表节点索引指针 */ 
+	MiniListItem_t xListEnd;              /* 链表最后一个节点 */
+}List_t;
+
+/* 链表精简节点结构体定义 */
+struct xMINI_LIST_ITEM
+{
+    TickType_t xItemValue;                /* 辅助值，用于帮助节点做升序排列 */
+	struct xLIST_ITEM * pxNext;           /* 指向链表下一个节点 */
+	struct xLIST_ITEM * pxPrevious;       /* 指向链表前一个节点 */
+};
+typedef struct xMINI_LIST_ITEM MiniListItem_t;   /* 精简节点数据类型重定义 */
+
+
+
+void vListInitialiseItem( ListItem_t * const pxItem );
+void vListInitialise( List_t * const pxList );
+
+
 
 #endif /* LIST_H */
--- freertos/list.c
+++ freertos/list.c
@@ -4,3 +4,21 @@ void vListInitialiseItem( ListItem_t * const pxItem )
 {
     pxItem->pvContainer = NULL;
 }
+
+
+/* 链表根节点初始化 */
+void vListInitialise( List_t * const pxList )
+{
+    /* 将链表索引指针指向最后一个节点 */ 
+    pxList->pxIndex = ( ListItem_t * ) & ( pxList->xListEnd );
+
+	/* 将链表最后一个节点的辅助排序的值设置为最大，确保该节点就是链表的最后节点 */
+	pxList->xListEnd.xItemValue = portMAX_DELAY;
+
+	/* 将最后一个节点的pxNext 和 pxPrevious 指针均指向节点自身，表示链表为空 */
+	pxList->xListEnd.pxNext = ( ListItem_t * ) & ( pxList->xListEnd );
+	pxList->xListEnd.pxPrevious = ( ListItem_t * ) & ( pxList->xListEnd );
+
+	/* 初始化链表节点计数器的值为0，表示链表为空 */
+	pxList->uxNumberOfItems = (UBaseType_t) 0U;
+}
